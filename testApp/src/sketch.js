58f73b0bef674aa820d1af4cb108a189072425d5
nari <nari@itu.dk>
2024-06-25 22:23:19 +0200

    Added files for the testApp

---
+++ testApp/src/sketch.js
@@ -0,0 +1,15 @@
+let bubble1;
+let bubble2;
+let ds;
+
+function setup() {
+  createCanvas(600, 400);
+  //ds = new PenroseLSystem();
+  //please, play around with the following line
+  //ds.simulate(5);
+}
+
+function draw() {
+  background(0);
+  //ds.render();
+}
